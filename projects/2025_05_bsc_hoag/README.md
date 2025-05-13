86be3f1723a41c9cae8cc1fdd0095891e56d5748
Dan Snow <dan@sno.ws>
2025-05-13 15:43:39 -0700

    Snapshot Hoag BSC queries

--- projects/2025_05_bsc_hoag/README.md
+++ projects/2025_05_bsc_hoag/README.md
@@ -1,11 +1,31 @@
 # Project template
 
-## Abstract
+Side note: Neither BS of CA or Hoag are customers
+What we want to do is look at the orange country payer / provider market and contextualize these on-going negotiations to help inform the conversation. Some obvious questions to explore:
 
-## Data used
+- Are Hoag’s payments from BC of CA in line with the market?
+- How does Hoag compare to the local market?
+- How does BC of CA compare to the local market?
+- Any other interesting trends or contextualization of the local market?
 
-## Methods
+## Notes
 
-## References
+- Includes CMS shoppable services
+- Triple check with the data team that the rates we pull are correct
+- Maybe start with hospital rates
+- Check for carveouts
 
-## Addenda
+## Competitors
+
+- UCI Medical Center
+- St Jude & St Joseph (providence)
+- Memorial Care
+
+## Todo
+
+- Dig into specific comparable hospitals to Hoag in Orange county
+- Look for a trend over time in BSC v Hoag rates. Try RC 272 (NICU per diem)
+- Look for unsustainable trend in rates
+- "I think you need ready access to hospital charge relativities (sometimes the out of the box index is the Cost to Charge Ratio CCR in the medicare cost report). That way you can keep an eye on when % of charge rates combined with list prices are out of whack. Chansoo and Stefano know more about this"
+- Directly compare BSC to Anthem within Hoag
+
---
+++ projects/2025_05_bsc_hoag/queries/rates.sql
@@ -0,0 +1,62 @@
+SELECT
+    state,
+    county,
+    zip_code,
+    cbsa,
+    cbsa_name,
+    city,
+    hq_latitude,
+    hq_longitude,
+    payer_name,
+    payer_id,
+    network_id,
+    network_name,
+    payer_network_name,
+    network_type,
+    network_class,
+    health_system_name,
+    provider_id,
+    provider_name,
+    total_beds,
+    bill_type,
+    facility,
+    billing_code_type,
+    billing_code,
+    canonical_rate_original_billing_codes,
+    canonical_rate_original_billing_code_type,
+    service_description,
+    service_line,
+    therapeutic_area,
+    is_drug_code,
+    is_surg_code,
+    asp_payment_limit,
+    medicare_rate,
+    medicare_pricing_type,
+    discounted_cash_rate,
+    canonical_gross_charge,
+    canonical_gross_charge_type,
+    canonical_rate,
+    canonical_rate_source,
+    canonical_rate_type,
+    canonical_rate_percent_of_medicare,
+    canonical_rate_percent_of_list,
+    canonical_rate_validation_method
+FROM tq_dev.internal_dev_csong_cld_v1_1.prod_combined_abridged
+WHERE cbsa_name = 'Los Angeles-Long Beach-Anaheim, CA'
+    AND canonical_rate IS NOT NULL
+    AND canonical_rate_source IN ('hospital', 'payer')
+    AND canonical_rate_validation_method != 'outlier'
+    AND canonical_rate_percent_of_medicare BETWEEN 0.5 AND 10
+    AND LOWER(provider_name) NOT LIKE '%children%'
+    AND LOWER(provider_name) NOT LIKE '%cancer%'
+    AND (
+        (
+            billing_code_type = 'MS-DRG'
+            AND billing_code IN ('807', '470', '216', '871')
+        ) OR (
+            billing_code_type = 'HCPCS'
+            AND billing_code IN (
+                '45378', '27130', '93452', '71045', '74177', '93005'
+            )
+        )
+    )
