4d4bd43851d8402ce9bd8aa959e191cc694679bb
Dan Snow <dan@sno.ws>
2025-06-18 09:58:32 -0700

    Add gross charges and hospital conversion factor

--- projects/2025_06_il_340b/queries/medicare_cost_reports.sql
+++ projects/2025_06_il_340b/queries/medicare_cost_reports.sql
@@ -8,6 +8,8 @@ SELECT
     c_1_c5_73 / (c_1_c6_73 + c_1_c7_73) AS mcr_drug_ccr,
     c_1_c7_73 / (c_1_c6_73 + c_1_c7_73) AS mcr_pct_outpatient,
     g3_c1_3 AS mcr_net_patient_revenue,
+    c_1_c6_202 + c_1_c7_202 AS mcr_gross_charges,
+    g3_c1_3 / (c_1_c6_202 + c_1_c7_202) AS mcr_conv_factor,
     e_a_hos_c1_33 AS mcr_dsh_pct
 FROM tq_dev.internal_dev_mnajarian.reference_medicare_cost_report_hosp10
 WHERE prvdr_num IN ( {{ ccn_values }} )
