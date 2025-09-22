dfc481ab57cf7bcca452d9c378ef4fac6a4fc339
Dan Snow <dan@sno.ws>
2025-09-22 15:01:24 -0700

    Working large pull

--- projects/2025_05_rural_v_urban/ingest.py
+++ projects/2025_05_rural_v_urban/ingest.py
@@ -1,3 +1,41 @@
+import random
+import string
+
+import polars as pl
+from dotenv import dotenv_values
 from tq.connectors import get_trino_connection
+from tq.utils import get_env_file_path
 
 trino_conn = get_trino_connection()
+config = dotenv_values(get_env_file_path())
+s3_base_uri = config.get("S3_BASE_URI", "")
+
+
+def load_table_with_ctas(
+    sql_file: str, s3_location: str | None, table_name: str
+) -> pl.DataFrame:
+    """Load table to Polars by first saving a SQL query to S3 as Parquet"""
+    if not s3_location or s3_location == "":
+        raise ValueError("s3_location must not be empty")
+
+    s3_full_path = f"s3://{s3_location}/{table_name}"
+    with open(sql_file, "r") as f:
+        sql = (
+            f.read()
+            .replace("{{ table_name }}", table_name)
+            .replace("{{ s3_location }}", s3_full_path)
+        )
+
+    with trino_conn.cursor() as cur:
+        cur.execute(sql)
+
+    return pl.read_parquet(s3_full_path + "/*")
+
+
+all_rates_df = load_table_with_ctas(
+    sql_file="queries/all_rates.sql",
+    s3_location=s3_base_uri,
+    table_name="dsnow_tmp_cld_rates_subset"
+    + "_"
+    + "".join(random.choices(string.ascii_lowercase + string.digits, k=5)),
+)
---
+++ projects/2025_05_rural_v_urban/queries/all_rates.sql
@@ -0,0 +1,64 @@
+CREATE TABLE hive.tmp.{{ table_name }}
+WITH (
+    external_location = '{{ s3_location }}', format = 'parquet'
+)
+AS
+SELECT
+    cld.roid,
+    cld.state,
+    cld.cbsa,
+    cld.cbsa_name,
+    cld.hq_latitude,
+    cld.hq_longitude,
+    cld.payer_id,
+    cld.payer_name,
+    cld.network_id,
+    cld.network_name,
+    cld.payer_network_name,
+    cld.network_type,
+    cld.network_class,
+    cld.health_system_id,
+    cld.health_system_name,
+    cld.provider_id,
+    cld.provider_name,
+    cld.taxonomy_grouping,
+    cld.provider_type,
+    cld.total_beds,
+    cld.bill_type,
+    cld.billing_code_type,
+    cld.billing_code,
+    cld.service_description,
+    cld.service_line,
+    cld.therapeutic_area,
+    cld.medicare_rate,
+    cld.medicare_pricing_type,
+    cld.medicare_reference_source,
+    cld.state_avg_medicare_rate,
+    cld.discounted_cash_rate,
+    cld.canonical_gross_charge,
+    cld.canonical_gross_charge_source,
+    cld.canonical_gross_charge_type,
+    cld.canonical_rate,
+    cld.canonical_rate_source,
+    cld.canonical_rate_type,
+    cld.canonical_rate_category,
+    cld.canonical_rate_class,
+    cld.canonical_rate_percent_of_medicare,
+    cld.canonical_rate_percent_of_state_avg_medicare,
+    cld.canonical_rate_percent_of_list,
+    cld.canonical_contract_methodology,
+    cld.canonical_rate_score
+FROM tq_dev.internal_dev_csong_cld_v2_1_1.prod_combined_abridged AS cld
+WHERE cld.taxonomy_grouping = 'Hospitals'
+    AND cld.network_type = 'PPO'
+    AND cld.canonical_rate IS NOT NULL
+    AND cld.canonical_rate_score >= 3
+    AND NOT cld.is_drug_code
+    AND (
+        -- No device rates, drug rates, etc
+        (
+            cld.billing_code_type = 'HCPCS'
+            AND REGEXP_LIKE(cld.billing_code, '^[0-9]{5}$')
+        )
+        OR cld.billing_code_type = 'MS-DRG'
+    )
