7ba1450dfc5b9782429c2218fc54e2aa2be5cc3a
Dan Snow <dan@sno.ws>
2025-09-26 14:39:11 -0700

    Checkpoint

--- projects/2025_05_rural_v_urban/ingest.py
+++ projects/2025_05_rural_v_urban/ingest.py
@@ -1,75 +1,62 @@
-import random
-import string
+# type: ignore
+import re
 
 import polars as pl
-from dotenv import dotenv_values
 from tq.connectors import get_trino_connection
-from tq.utils import get_env_file_path
 
 trino_conn = get_trino_connection()
-config = dotenv_values(get_env_file_path())
-s3_base_uri = config.get("S3_BASE_URI", "")
 
 
-###### Functions ###############################################################
+# Mini-classes for attaching utility methods to Polars DataFrames, mostly for
+# cleaning up OPAIS data
+@pl.api.register_dataframe_namespace("util")
+class UtilDataFrame:
+    def __init__(self, df: pl.DataFrame) -> None:
+        self._df = df
 
-
-@pl.api.register_expr_namespace("util")
-class UtilExpr:
-    def __init__(self, expr: pl.Expr) -> None:
-        self._expr = expr
-
-    def wmean(self, weight: str) -> pl.Expr:
-        """
-        Compute the weighted mean for an expression, given a weight column name.
-        Usage: pl.col("value_col").util.weighted_mean("weight_col")
-        """
-        weights = pl.when(self._expr.is_not_null()).then(pl.col(weight))
-        return weights.dot(self._expr).truediv(weights.sum()).fill_nan(None)
-
-
-def load_table_with_ctas(
-    sql_file: str, s3_location: str | None, table_name: str
-) -> pl.DataFrame:
-    """
-    Load table to Polars by first saving a SQL query to S3 as a Parquet file.
-
-    This is way faster than just directly loading large query results using the
-    Trino client.
-    """
-    if not s3_location or s3_location == "":
-        raise ValueError("s3_location must not be empty")
-
-    s3_full_path = f"s3://{s3_location}/{table_name}"
-    with open(sql_file, "r") as f:
-        sql = (
-            f.read()
-            .replace("{{ table_name }}", table_name)
-            .replace("{{ s3_location }}", s3_full_path)
+    def to_snake_case(self: pl.DataFrame) -> pl.DataFrame:
+        """Convert all column names to snake_case."""
+        return self._df.rename(
+            {
+                col: re.sub(r"[^a-zA-Z0-9]", "_", col.lower())
+                for col in self._df.columns
+            }
         )
 
-    with trino_conn.cursor() as cur:
-        cur.execute(sql)
-
-    return pl.read_parquet(s3_full_path + "/*")
+    def empty_strings_to_null(self: pl.DataFrame) -> pl.DataFrame:
+        """Convert all empty string columns to null."""
+        return self._df.with_columns(
+            pl.when(pl.col(pl.String).str.len_chars() == 0)
+            .then(None)
+            .otherwise(pl.col(pl.String))
+            .name.keep()
+        )
 
 
 ###### Data loading ############################################################
 
-# Grab basically all hospital rates for the entire United States
-rates_df = load_table_with_ctas(
-    sql_file="queries/rates.sql",
-    s3_location=s3_base_uri,
-    table_name="dsnow_tmp_cld_rates_subset"
-    + "_"
-    + "".join(random.choices(string.ascii_lowercase + string.digits, k=5)),
+# Load the aggregated rates data via big ol' SQL query
+with open("queries/rates.sql", "r") as query:
+    rates_df = pl.read_database(query.read(), trino_conn)
+
+# Load and cleanup the HRSA OPAIS data to get CAH status for each hospital
+opais_ce_df = (
+    pl.read_excel(
+        source="data/input/340b_opais.xlsx",
+        sheet_name="Covered Entities",
+        read_options={"header_row": 3},
+    )
+    .util.to_snake_case()
+    .util.empty_strings_to_null()
+    .with_columns((pl.col("participating") == "TRUE").alias("participating"))
+    .filter(pl.col("participating"))
+    .select(["medicare_provider_number", "entity_type"])
+    .rename({"entity_type": "opais_340b_entity_type"})
+    .unique()
 )
 
-# Grab state-specific code utilization data (procedure counts)
-with open("queries/code_utilization.sql", "r") as query:
-    code_utilization_df = pl.read_database(query.read(), trino_conn)
-
 # Read county-level crosswalk for translating TQ state-county names to GEOIDs
+# Created this manually with Claude + some hand matching
 county_xwalk_df = pl.read_csv(
     source="data/input/county_crosswalk.csv",
     schema_overrides={
@@ -80,6 +67,7 @@ county_xwalk_df = pl.read_csv(
 )
 
 # Read NCHS urban-rural classification codes
+# https://www.cdc.gov/nchs/data-analysis-tools/urban-rural.html
 nchs_df = (
     pl.read_csv(
         source="data/input/NCHSurb-rural-codes.csv",
@@ -110,6 +98,7 @@ nchs_df = (
 )
 
 # Read USDS rural-urban continuum codes
+# https://www.ers.usda.gov/data-products/rural-urban-continuum-codes
 usds_df = (
     (
         pl.read_csv(
@@ -132,39 +121,10 @@ usds_df = (
 )
 
 
-###### Data cleaning ###########################################################
-
-# Add the payer percentile rank to use as weights. Some rows are missing payer
-# info, so we just share the most common value across rows by payer
-rates_clean_df = (
-    rates_df.with_columns(
-        pl.col("national_payer_covered_lives")
-        .fill_null(pl.col("national_payer_covered_lives").mode())
-        .over("payer_id")
-    )
-    .with_columns(
-        (
-            (pl.col("national_payer_covered_lives").rank(method="dense") - 1)
-            / (pl.col("national_payer_covered_lives").unique().len() - 1)
-        ).alias("national_payer_covered_lives_percentile")
-    )
-    .with_columns(pl.col("national_payer_covered_lives").fill_null(0.01))
-)
-
-# Attach state-specific code utilization data. If missing (fairly common),
-# assume extremely low utilization (1% percentile rank)
-rates_clean_df = rates_clean_df.join(
-    code_utilization_df,
-    on=["state", "billing_code_type", "billing_code"],
-    how="left",
-).with_columns(
-    pl.col("state_claims_percentile_sep").fill_null(0.01),
-    pl.col("state_claims_percentile_all").fill_null(0.01),
-)
-
-# Join county codes and rural-urban classifications
-rates_clean_df = (
-    rates_clean_df.join(
+# Join county codes, rural-urban classifications, and CAH status, then save
+# to disk
+(
+    rates_df.join(
         county_xwalk_df,
         on=["state", "county"],
         how="left",
@@ -181,23 +141,10 @@ rates_clean_df = (
         right_on="geoid",
         how="left",
     )
-)
-
-rates_clean_df.group_by(["payer_id", "provider_id"]).agg(
-    pl.col("canonical_rate_percent_of_medicare")
-    .util.wmean("state_claims_percentile_all")
-    .alias("rate_mean"),
-    pl.len().alias("num_rates"),
-    pl.col("national_payer_covered_lives_percentile").first(),
-    pl.col("total_beds").first(),
-    pl.col("usds_class").first(),
-).group_by(["provider_id"]).agg(
-    pl.col("rate_mean")
-    .util.wmean("national_payer_covered_lives_percentile")
-    .alias("rate_mean"),
-    pl.col("num_rates").sum().alias("num_rates"),
-    pl.col("total_beds").first(),
-    pl.col("usds_class").first(),
-).group_by(["usds_class"]).agg(
-    pl.col("num_rates").sum().alias("num_rates"), pl.col("rate_mean").mean()
-)
+    .join(
+        opais_ce_df,
+        left_on="medicare_provider_id",
+        right_on="medicare_provider_number",
+        how="left",
+    )
+).write_parquet("data/output/rates_clean.parquet")
--- projects/2025_05_rural_v_urban/queries/rates.sql
+++ projects/2025_05_rural_v_urban/queries/rates.sql
@@ -1,55 +1,341 @@
-CREATE TABLE hive.tmp.{{ table_name }}
-WITH (
-    external_location = '{{ s3_location }}', format = 'parquet'
+-- Keep only commercial rates from hospitals, no MA, no drug codes, no ASCs, etc
+WITH cld_subset AS (
+    SELECT cld.*
+    FROM tq_dev.internal_dev_csong_cld_v2_1_1.prod_combined_all AS cld
+    WHERE cld.taxonomy_grouping = 'Hospitals'
+        AND cld.network_type = 'PPO'
+        AND cld.canonical_rate IS NOT NULL
+        AND cld.canonical_rate_score >= 3
+        AND NOT cld.is_drug_code
+        AND (
+            -- No device rates, drug rates, etc
+            (
+                cld.billing_code_type = 'HCPCS'
+                AND REGEXP_LIKE(cld.billing_code, '^[0-9]{5}$')
+            )
+            OR cld.billing_code_type = 'MS-DRG'
+        )
+        AND cld.canonical_rate_percent_of_medicare BETWEEN 0.4 AND 10.0
+),
+
+-- Use Policy Reporter to get state-level number of covered lives per payer,
+-- then turn that into a 0-1 weight using a percentile rank
+policy_reporter AS (
+    SELECT
+        pr.line_of_business,
+        pr.plan_type,
+        pr.state_short AS state,
+        pr.covered_lives,
+        pr.tq_payer_payer_id
+    FROM redshift.reference.policy_reporter_state AS pr
+    WHERE pr.line_of_business = 'Commercial'
+),
+
+state_payer_total AS (
+    SELECT
+        line_of_business,
+        state,
+        tq_payer_payer_id,
+        SUM(covered_lives) AS payer_covered_lives
+    FROM policy_reporter
+    GROUP BY line_of_business, state, tq_payer_payer_id
+),
+
+state_total AS (
+    SELECT
+        line_of_business,
+        state,
+        SUM(covered_lives) AS state_covered_lives
+    FROM policy_reporter
+    GROUP BY line_of_business, state
+),
+
+payer_market_share AS (
+    SELECT DISTINCT
+        pr.state,
+        pr.line_of_business,
+        pr.tq_payer_payer_id AS payer_id,
+        PERCENT_RANK()
+            OVER (
+                PARTITION BY pr.state
+                ORDER BY spt.payer_covered_lives / st.state_covered_lives
+            )
+            AS state_market_share
+    FROM policy_reporter AS pr
+    LEFT JOIN state_payer_total AS spt
+        ON pr.state = spt.state
+        AND pr.tq_payer_payer_id = spt.tq_payer_payer_id
+        AND pr.line_of_business = spt.line_of_business
+    LEFT JOIN state_total AS st
+        ON pr.state = st.state
+        AND pr.line_of_business = st.line_of_business
+),
+
+-- Turn claim counts into a 0-1 percentile rank by state. Used to downweight
+-- codes that aren't heavily used
+code_utilization AS (
+    SELECT DISTINCT
+        *,
+        PERCENT_RANK()
+            OVER (
+                PARTITION BY state, billing_code_type
+                ORDER BY state_claims_count
+            )
+            AS state_claims_percentile_sep,
+        PERCENT_RANK()
+            OVER (
+                PARTITION BY state
+                ORDER BY state_claims_count
+            )
+            AS state_claims_percentile_all
+    FROM (
+        SELECT
+            state,
+            billing_code,
+            billing_code_type,
+            SUM(total_count_encounters) AS state_claims_count
+        FROM tq_production.claims_benchmarks.claims_benchmarks_utilization_state
+        WHERE REGEXP_LIKE(billing_code, '^[0-9]{3}$|^[0-9]{5}')
+            AND service_year = 2024
+        GROUP BY state, billing_code, billing_code_type
+    )
+),
+
+-- Join the weights data and replace missing weights with a 1st
+-- percentile rank (lowest)
+cld_filled AS (
+    SELECT
+        cs.*,
+        COALESCE(cu.state_claims_percentile_sep, 0.01)
+            AS state_claims_percentile_sep,
+        COALESCE(cu.state_claims_percentile_all, 0.01)
+            AS state_claims_percentile_all,
+        COALESCE(pm.state_market_share, 0.01) AS state_market_share
+    FROM cld_subset AS cs
+    LEFT JOIN code_utilization AS cu
+        ON cs.state = cu.state
+        AND cs.billing_code = cu.billing_code
+        AND cs.billing_code_type = cu.billing_code_type
+    LEFT JOIN payer_market_share AS pm
+        ON cs.state = pm.state
+        AND cs.payer_id = pm.payer_id
+),
+
+-- Extract provider-level information into its own CTE so we don't have to
+-- carry it through all the aggregations later
+provider_info AS (
+    SELECT DISTINCT
+        provider_id,
+        provider_name,
+        health_system_id,
+        health_system_name,
+        medicare_provider_id,
+        provider_hospital_ownership,
+        provider_geographic_classification,
+        provider_340b_hospital_type,
+        provider_net_patient_revenue,
+        provider_cms_overall_rating,
+        total_beds,
+        nashp_medicare_mix,
+        nashp_medicaid_mix,
+        nashp_commercial_payer_mix
+    FROM cld_filled
+),
+
+-- Aggregate payer-provider-code level rates to the payer-provider level,
+-- weighting by the code utilization percentiles
+agg_payer_provider AS (
+    SELECT
+        state,
+        county,
+        provider_id,
+        provider_name,
+        payer_id,
+        ARBITRARY(state_market_share) AS state_market_share,
+        COUNT() AS num_rates,
+        AVG(canonical_rate) AS mean_rate,
+        AVG(canonical_rate_percent_of_medicare) AS mean_pct_of_medicare,
+        SUM(canonical_rate * state_claims_percentile_all)
+        / SUM(state_claims_percentile_all) AS wtd_mean_rate,
+        SUM(
+            canonical_rate_percent_of_medicare
+            * state_claims_percentile_all
+        )
+        / SUM(state_claims_percentile_all)
+            AS wtd_mean_pct_of_medicare
+    FROM cld_filled
+    -- Keep only payer-provider combos with more than 500 rates across all codes
+    -- Fewer than this tends to introduce a lot of noise
+    GROUP BY 1, 2, 3, 4, 5 HAVING COUNT() >= 500 -- noqa
+),
+
+-- Aggregate the payer-provider level data to the provider level, weighting
+-- payers by their state market share
+agg_provider AS (
+    SELECT
+        state,
+        county,
+        provider_id,
+        SUM(num_rates) AS num_rates,
+        AVG(mean_rate) AS mean_rate,
+        AVG(mean_pct_of_medicare) AS mean_pct_of_medicare,
+        SUM(wtd_mean_rate * state_market_share)
+        / SUM(state_market_share) AS wtd_mean_rate,
+        SUM(wtd_mean_pct_of_medicare * state_market_share)
+        / SUM(state_market_share) AS wtd_mean_pct_of_medicare
+    FROM agg_payer_provider
+    GROUP BY 1, 2, 3 -- noqa
+),
+
+-- Same aggregations as above, but this time broken out by setting/type
+agg_payer_provider_type AS (
+    SELECT
+        state,
+        county,
+        provider_id,
+        provider_name,
+        payer_id,
+        billing_code_type,
+        ARBITRARY(state_market_share) AS state_market_share,
+        COUNT() AS num_rates,
+        AVG(canonical_rate) AS mean_rate,
+        AVG(canonical_rate_percent_of_medicare) AS mean_pct_of_medicare,
+        SUM(canonical_rate * state_claims_percentile_all)
+        / SUM(state_claims_percentile_all) AS wtd_mean_rate,
+        SUM(
+            canonical_rate_percent_of_medicare
+            * state_claims_percentile_all
+        )
+        / SUM(state_claims_percentile_all)
+            AS wtd_mean_pct_of_medicare
+    FROM cld_filled
+    -- Lower code count threshold since we've now split by setting
+    GROUP BY 1, 2, 3, 4, 5, 6 HAVING COUNT() >= 100 -- noqa
+),
+
+agg_provider_type AS (
+    SELECT
+        state,
+        county,
+        provider_id,
+        billing_code_type,
+        SUM(num_rates) AS num_rates,
+        AVG(mean_rate) AS mean_rate,
+        AVG(mean_pct_of_medicare) AS mean_pct_of_medicare,
+        SUM(wtd_mean_rate * state_market_share)
+        / SUM(state_market_share) AS wtd_mean_rate,
+        SUM(wtd_mean_pct_of_medicare * state_market_share)
+        / SUM(state_market_share) AS wtd_mean_pct_of_medicare
+    FROM agg_payer_provider_type
+    GROUP BY 1, 2, 3, 4 -- noqa
+),
+
+-- Same aggregations as above, but now with a subset of representative codes
+agg_payer_provider_subset AS (
+    SELECT
+        state,
+        county,
+        provider_id,
+        provider_name,
+        payer_id,
+        ARBITRARY(state_market_share) AS state_market_share,
+        COUNT() AS num_rates,
+        AVG(canonical_rate) AS mean_rate,
+        AVG(canonical_rate_percent_of_medicare) AS mean_pct_of_medicare
+    FROM cld_filled
+    WHERE (
+        billing_code_type = 'HCPCS'
+        AND billing_code IN (
+            '27130',
+            '29881',
+            '29877',
+            '42820',
+            '66984',
+            '71046',
+            '73720',
+            '73721',
+            '77066'
+        )
+    ) OR (
+        billing_code_type = 'MS-DRG'
+        AND billing_code IN (
+            '177',
+            '195',
+            '280',
+            '291',
+            '343',
+            '460',
+            '470',
+            '743',
+            '788',
+            '807',
+            '871'
+        )
+    )
+    -- Each payer-provider combo should have at least 1/3 of the total codes
+    GROUP BY 1, 2, 3, 4, 5 HAVING COUNT() >= 10 -- noqa
+),
+
+agg_provider_subset AS (
+    SELECT
+        state,
+        county,
+        provider_id,
+        SUM(num_rates) AS num_rates,
+        AVG(mean_rate) AS mean_rate,
+        AVG(mean_pct_of_medicare) AS mean_pct_of_medicare,
+        SUM(mean_rate * state_market_share)
+        / SUM(state_market_share) AS wtd_mean_rate,
+        SUM(mean_pct_of_medicare * state_market_share)
+        / SUM(state_market_share) AS wtd_mean_pct_of_medicare
+    FROM agg_payer_provider_subset
+    GROUP BY 1, 2, 3 -- noqa
 )
-AS
 
 SELECT
-    cld.roid,
-    cld.state,
-    cld.county,
-    cld.hq_latitude,
-    cld.hq_longitude,
-    cld.payer_id,
-    cld.payer_name,
-    cld.network_id,
-    cld.network_name,
-    cld.payer_network_name,
-    cld.network_type,
-    cld.network_class,
-    cld.health_system_id,
-    cld.health_system_name,
-    cld.provider_id,
-    cld.provider_name,
-    cld.provider_type,
-    cld.total_beds,
-    cld.bill_type,
-    cld.billing_code_type,
-    cld.billing_code,
-    cld.medicare_rate,
-    cld.discounted_cash_rate,
-    cld.canonical_gross_charge,
-    cld.canonical_rate,
-    cld.canonical_rate_source,
-    cld.canonical_rate_type,
-    cld.canonical_rate_category,
-    cld.canonical_rate_class,
-    cld.canonical_rate_percent_of_medicare,
-    cld.canonical_rate_percent_of_list,
-    cld.canonical_contract_methodology,
-    cld.canonical_rate_score,
-    cld.national_payer_covered_lives
-FROM tq_dev.internal_dev_csong_cld_v2_1_1.prod_combined_abridged AS cld
-WHERE cld.taxonomy_grouping = 'Hospitals'
-    AND cld.network_type = 'PPO'
-    AND cld.canonical_rate IS NOT NULL
-    AND cld.canonical_rate_score >= 3
-    AND NOT cld.is_drug_code
-    AND (
-        -- No device rates, drug rates, etc
-        (
-            cld.billing_code_type = 'HCPCS'
-            AND REGEXP_LIKE(cld.billing_code, '^[0-9]{5}$')
-        )
-        OR cld.billing_code_type = 'MS-DRG'
-    )
+    ap.state,
+    ap.county,
+    pi.*,
+    ap.num_rates AS all_num_rates,
+    ap.mean_rate AS all_mean_rate,
+    ap.mean_pct_of_medicare AS all_mean_pct_of_medicare,
+    ap.wtd_mean_rate AS all_wtd_mean_rate,
+    ap.wtd_mean_pct_of_medicare AS all_wtd_mean_pct_of_medicare,
+    ip.num_rates AS ip_num_rates,
+    ip.mean_rate AS ip_mean_rate,
+    ip.mean_pct_of_medicare AS ip_mean_pct_of_medicare,
+    ip.wtd_mean_rate AS ip_wtd_mean_rate,
+    ip.wtd_mean_pct_of_medicare AS ip_wtd_mean_pct_of_medicare,
+    op.num_rates AS op_num_rates,
+    op.mean_rate AS op_mean_rate,
+    op.mean_pct_of_medicare AS op_mean_pct_of_medicare,
+    op.wtd_mean_rate AS op_wtd_mean_rate,
+    op.wtd_mean_pct_of_medicare AS op_wtd_mean_pct_of_medicare,
+    sub.num_rates AS sub_num_rates,
+    sub.mean_rate AS sub_mean_rate,
+    sub.mean_pct_of_medicare AS sub_mean_pct_of_medicare,
+    sub.wtd_mean_rate AS sub_wtd_mean_rate,
+    sub.wtd_mean_pct_of_medicare AS sub_wtd_mean_pct_of_medicare
+FROM agg_provider AS ap
+LEFT JOIN
+    (
+        SELECT * FROM agg_provider_type
+        WHERE billing_code_type = 'HCPCS'
+    ) AS op
+    ON ap.state = op.state
+    AND ap.county = op.county
+    AND ap.provider_id = op.provider_id
+LEFT JOIN
+    (
+        SELECT * FROM agg_provider_type
+        WHERE billing_code_type = 'MS-DRG'
+    ) AS ip
+    ON ap.state = ip.state
+    AND ap.county = ip.county
+    AND ap.provider_id = ip.provider_id
+LEFT JOIN agg_provider_subset AS sub
+    ON ap.state = sub.state
+    AND ap.county = sub.county
+    AND ap.provider_id = sub.provider_id
+LEFT JOIN provider_info AS pi
+    ON ap.provider_id = pi.provider_id
